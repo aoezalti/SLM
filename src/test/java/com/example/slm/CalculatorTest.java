ec5fb658d2966c57a3aac545d4b590c08fd844be
aoezalti <ayse.oezaltin@outlook.com>
2022-06-22 16:45:15 +0200

    added convert function to convert inches to centimeters

---
+++ src/test/java/com/example/slm/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example.slm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void convertInchesToCm() {
+    }
+}
