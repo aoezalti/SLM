2e03d3cc91cfeb3a65227b00803d975c6deeed0c
aoezalti <ayse.oezaltin@outlook.com>
2022-06-22 15:47:59 +0200

    added divide function

--- src/main/java/com/example/slm/Calculator.java
+++ src/main/java/com/example/slm/Calculator.java
@@ -1,7 +1,13 @@
 package com.example.slm;
 
 public class Calculator {
-    public int sum(int a,  int b) {
+    public int sum(int a, int b) {
         return a + b;
     }
+
+
+    public int division(int a, int b) {
+        return Math.floorDiv(a, b);
+    }
+
 }
